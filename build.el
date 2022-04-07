e60c78b4002a2381346212b3cbf1d9f4d0ab2f05
Matej Focko <mfocko@redhat.com>
2022-04-07 20:37:03 +0200

    chore: set up build system for org → html
    - add ‹*.html› to .gitignore
    - create build script in elisp
    - add CSS
    
    Signed-off-by: Matej Focko <mfocko@redhat.com>

---
+++ build.el
@@ -0,0 +1,11 @@
+(setq org-publish-project-alist
+      '(("org"
+         :base-directory "~/git/xfocko/ib002"
+         :publishing-function org-html-publish-to-html
+         :publishing-directory "~/git/xfocko/ib002"
+         :recursive t
+         :section-numbers nil
+         :with-toc 2
+         :html-head "<link rel=\"stylesheet\"
+                  href=\"/css/org.css\" type=\"text/css\"/>"
+         :html-preamble t)))
